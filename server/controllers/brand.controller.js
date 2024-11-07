1f81116ff0721df22abff495b9e976397c193d0b
pasha <majid.pashayi443earlier@gmail.com>
2024-11-07 14:27:54 +0330

    delete comment

--- server/controllers/brand.controller.js
+++ server/controllers/brand.controller.js
@@ -1,17 +1,3 @@
-/**
- * Title: Write a program using JavaScript on Brand Controller
- * Author: Hasibul Islam
- * Portfolio: https://devhasibulislam.vercel.app
- * Linkedin: https://linkedin.com/in/devhasibulislam
- * GitHub: https://github.com/devhasibulislam
- * Facebook: https://facebook.com/devhasibulislam
- * Instagram: https:/instagram.com/devhasibulislam
- * Twitter: https://twitter.com/devhasibulislam
- * Pinterest: https://pinterest.com/devhasibulislam
- * WhatsApp: https://wa.me/8801906315901
- * Telegram: devhasibulislam
- * Date: 11, November 2023
- */
 
 /* internal import */
 const brandService = require("../services/brand.service");
