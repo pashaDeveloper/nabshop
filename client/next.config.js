eb3f3c1516449206c836d948c0e3ce97d313d4ae
pasha <majid.pashayi443earlier@gmail.com>
2025-02-15 13:52:15 +0330

    admit upload domain

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'], 
+    domains: ['localhost',"https://s3-console.noghlenab.com"], 
     remotePatterns: [
       {
         protocol: "https",
