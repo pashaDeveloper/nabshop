6ed1fc61860b0bc039fecb2bb5cdb2762024bf67
Dotadotnet <Dotadotnet@outlook.com>
2025-02-17 19:38:28 +0330

    22

--- client/src/components/details/CartButton.js
+++ client/src/components/details/CartButton.js
@@ -118,7 +118,7 @@ const CartButton = ({ product }) => {
                 {selectedUnit?.unit._id === variation.unit._id ? <TiTickOutline className="w-8 h-8 text-white" /> :"" }      
                   {/* Tooltip */}
                   <div
-                    className={`absolute top-full right-1/2 -translate-x-1/2 mt-3  px-2 py-1 z-50 bg-secondary text-white text-sm rounded shadow-lg transition-opacity duration-300  whitespace-nowrap
+                    className={`absolute top-full right-1/2 -translate-x-1/2 mt-3  px-2 py-1  bg-secondary text-white text-sm rounded shadow-lg transition-opacity duration-300  whitespace-nowrap
                   ${
                     selectedUnit?.unit._id === variation.unit._id
                       ? "opacity-100 visible"
