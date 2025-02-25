d241dd5265d0fc69b4dcb846f8c7e34b964d3714
Dotadotnet <Dotadotnet@outlook.com>
2025-02-25 22:08:39 +0330

    add blog

--- client/src/components/home/blogs/Blog.js
+++ client/src/components/home/blogs/Blog.js
@@ -1,8 +1,12 @@
 // import Image from "next/image";
 import Container from "@/components/shared/Container";
 import HighlightText from "@/components/shared/highlightText/HighlightText";
-import React from "react";
-import Blogs from "./Blogs"
+import React, { useMemo } from "react";
+import { useGetBlogsQuery } from "@/services/blog/blogApi";
+import BlogCard from "@/components/shared/card/BlogCard";
+const { isLoading, data } = useGetBlogsQuery();
+const blogs = useMemo(() => data?.data || [], [data]);
+
 const Blog = () => {
   return (
     <section
@@ -27,8 +31,15 @@ const Blog = () => {
             </p>
           </article>
           <div>
-            <Blogs />
-          </div>
+          <section className="grid grid-cols-1 gap-4 sm:grid-cols-2  lg:grid-cols-3 ">
+        {isLoading || blogs.length === 0
+          ? Array.from({ length: 4 }, (_, index) => (
+            <BlogCard key={index}  />
+            ))
+          : blogs.map((blog, index) => <BlogCard key={index} blog={blog} isLoading={isLoading} />
+          
+          )}
+      </section>          </div>
         </div>
       </Container>
     </section>
