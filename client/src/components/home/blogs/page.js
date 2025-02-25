720ce126c75b45487ef33101080b25e977619e97
Dotadotnet <Dotadotnet@outlook.com>
2025-02-25 23:51:35 +0330

    delete blogcard

--- client/src/components/home/blogs/page.js
+++ client/src/components/home/blogs/page.js
@@ -3,7 +3,6 @@ import Container from "@/components/shared/Container";
 import HighlightText from "@/components/shared/highlightText/HighlightText";
 import React from "react";
 import { useGetBlogsQuery } from "@/services/blog/blogApi";
-import BlogCard from "@/components/shared/card/BlogCard";
 import BlogCardSkeleton from "@/components/shared/skeletonLoading/BlogCardSkeleton";
 
 const Blog = () => {
