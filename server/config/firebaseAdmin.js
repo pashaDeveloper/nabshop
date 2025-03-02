4da3136ceacc483ee60b4ab52ccb47b251ec8d1a
Dotadotnet <Dotadotnet@outlook.com>
2025-03-02 21:21:59 +0330

    firebase chnage

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.GOOGLE_CLOUD_CREDENTIALS);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
